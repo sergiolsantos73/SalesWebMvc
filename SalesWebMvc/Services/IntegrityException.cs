2b7be83dacb39dff9155980af408ea053717462e
Sergio Luiz dos Santos <sergioluiz73@gmail.com>
2021-08-24 10:33:31 -0300

    Integridade Delete

---
+++ SalesWebMvc/Services/IntegrityException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMvc.Services
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message) { }
+    }
+}
